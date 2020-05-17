53e21ab52a14e99c1b8a5211d6cb7d98451153e8
Hung Nguyen <happyhung@yahoo.com>
2020-05-17 23:31:41 +0300

    Updates

---
+++ precache-manifest.ea6ffe9a9bdfb1ff19dab0705328c584.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df2264de802d644a66efe3aeec81ad9",
+    "url": "/ft4-react-assignment-1/index.html"
+  },
+  {
+    "revision": "29e15b8839be6de6b9a0",
+    "url": "/ft4-react-assignment-1/static/css/main.4b14cf9c.chunk.css"
+  },
+  {
+    "revision": "5d5bee74310c1409ec96",
+    "url": "/ft4-react-assignment-1/static/js/2.7a3a4ad8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ft4-react-assignment-1/static/js/2.7a3a4ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e15b8839be6de6b9a0",
+    "url": "/ft4-react-assignment-1/static/js/main.07bde5bb.chunk.js"
+  },
+  {
+    "revision": "7d443b4372ccc0eecefb",
+    "url": "/ft4-react-assignment-1/static/js/runtime-main.b809cb17.js"
+  },
+  {
+    "revision": "ab6575ad44c62fdd10f134e64a5494f7",
+    "url": "/ft4-react-assignment-1/static/media/img-1.ab6575ad.jpg"
+  },
+  {
+    "revision": "121d63bb46998903f943a509f8a75ebd",
+    "url": "/ft4-react-assignment-1/static/media/img-2.121d63bb.jpg"
+  },
+  {
+    "revision": "b458a2709439626a2e68177026fa5a08",
+    "url": "/ft4-react-assignment-1/static/media/img-3.b458a270.jpg"
+  },
+  {
+    "revision": "7408cbace38cf52ebf2eb29f9ef5e44b",
+    "url": "/ft4-react-assignment-1/static/media/intro.7408cbac.jpg"
+  }
+]);
